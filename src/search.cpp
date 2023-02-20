f92aecb54d6ee9a94f085d860615c1fe0f9e0aa4
われ <74850252+ware255@users.noreply.github.com>
2023-02-20 19:44:17 +0900

    Add files via upload

--- src/search.cpp
+++ src/search.cpp
@@ -2,8 +2,10 @@
 
 search::search(int t) {
     p = t;
+    /* 文字化け対策 */
     system("chcp 65001");
     system("cls");
+    /* AA表示 */
     std::cout << "\n  __   ___    __    ___    ___  _  _          ___   ___   ___   ___   ___    ___  _____" << std::endl;
     std::cout << "/' _/ | __|  /  \\  | _ \\  / _/ | || |        | _,\\ | __| | _ \\ | __| | __|  / _/ |_   _|" << std::endl;
     std::cout << "`._`. | _|  | /\\ | | v / | \\__ | >< |  ____  | v_/ | _|  | v / | _|  | _|  | \\__   | |" << std::endl;
@@ -11,11 +13,16 @@ search::search(int t) {
 
 }
 
+/* アクセスできないディレクトリを判別 */
 int search::prohibited_directory(std::string str) {
     if (str != K01 && str != K02 && str != K03 && str != K04 && str != K05 && str != K06 && str != K07 &&
         str != K08 && str != K09 && str != K10 && str != K11 && str != K12 && str != K13 && str != K14 &&
         str != K15 && str != K16 && str != K17 && str != K18 && str != K19 && str != K20 && str != K21 &&
-        str != K22)
+        str != K22 && str != K23 && str != K24 && str != K25 && str != K26 && str != K27 && str != K28 &&
+        str != K29 && str != K30 && str != K31 && str != K32 && str != K33 && str != K34 && str != K35 &&
+        str != K36 && str != K37 && str != K38 && str != K39 && str != K40 && str != K41 && str != K42 &&
+        str != K43 && str != K44 && str != K45 && str != K46 && str != K47 && str != K48 && str != K49 &&
+        str != K50 && str != K51 && str != K52 && str != K53 && str != K54)
     {
         return 0;
     }
@@ -43,6 +50,7 @@ bool search::main() {
     std::cin >> file_name;
     std::cout << "\nI'm looking for a file.\n" << std::endl;
     start(L"C:\\Users");
+    start(L"C:\\Windows");
     if (p != 0) return true;
     return false;
 }
--- src/search.hpp
+++ src/search.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <filesystem>
 
+/* アクセスできないディレクトリたち */
 #define K01 "Application Data"
 #define K02 "All Users"
 #define K03 "History"
@@ -24,6 +25,38 @@
 #define K20 "SendTo"
 #define K21 "Templates"
 #define K22 "スタート メニュー"
+#define K23 "Start Menu"
+#define K24 "Programs"
+#define K25 "CSC"
+#define K26 "LiveKernelReports"
+#define K27 "SystemRestore"
+#define K28 "ModemLogs"
+#define K29 "Prefetch"
+#define K30 "Autopilot"
+#define K31 "VSCache"
+#define K32 "audit"
+#define K33 "cap"
+#define K34 "LocalService"
+#define K35 "NetworkService"
+#define K36 "ServiceState"
+#define K37 "dmp"
+#define K38 "config"
+#define K39 "Configuration"
+#define K40 "DriverData"
+#define K41 "DriverState"
+#define K42 "ias"
+#define K43 "RtBackup"
+#define K44 "MsDtc"
+#define K45 "networklist"
+#define K46 "SleepStudy"
+#define K47 "PRINTERS"
+#define K48 "SERVERS"
+#define K49 "sru"
+#define K50 "Tasks"
+#define K51 "MOF"
+#define K52 "WDI"
+#define K53 "SystemTemp"
+#define K54 "Msdtc"
 
 class search{
 private:
